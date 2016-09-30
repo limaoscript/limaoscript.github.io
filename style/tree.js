53a370d1f364bb992828466ae7ad9c1d87d6ad8f
jimmy <jimmy@bu2trip.com>
2016-09-30 09:02:54 +0800

    添加百度IFEtask25学习的树形结构demo

---
+++ style/tree.js
@@ -0,0 +1,163 @@
+	function Node(data) {
+		this.data = data;
+		this.parent = null;
+		this.children = [];
+		this.rootDOM = document.getElementById('tree-area');
+		this.selfDOM = null;
+	}
+
+	Node.prototype = {
+
+		render: function(arrow, visibility, setHightlight, deHightLight) {
+
+		},
+
+		isLeaf: function() {
+			return this.children.length == 0;
+		},
+		//展开/收拢
+		toggleFold: function() {
+			var i = 0,
+				len = this.children.length;
+			if (this.isLeaf()) return this;
+			for (; i < len; i++) {
+				this.children[i].render(false, true);
+			}
+			this.render(true, false);
+			return this;
+		},
+
+		//生成并显示树的DOM
+		showDOM: function(treeName, parent, self) {
+			var ul = document.createElement('ul'),
+				li = document.createElement('li'),
+				span = document.createElement('span');
+			span.innerHTML = treeName;
+			li.appendChild(span);
+			ul.appendChild(li);
+
+			if (!parent) {
+				ul.setAttribute('id', 'ul-root');
+				this.rootDOM.appendChild(ul);
+				this.selfDOM = ul;
+			} else {
+				parent.selfDOM.appendChild(ul);
+				self.selfDOM = ul;
+			}
+		},
+		//删除DOM
+		delDOM: function(parent, index) {
+			parent.selfDOM.removeChild(parent.children[index].selfDOM);
+		}
+	}
+
+	function Tree(data) {
+		this._root = new Node(data);
+		this._root.showDOM(data);
+	}
+
+	Tree.prototype = {
+
+		// //渲染
+		// render: function() {
+
+		// },
+
+		//深度首次遍历
+		traverseDF: function(callback) {
+			(function recurse(currentNode) {
+				for (var i = 0; i < currentNode.children.length; i++) {
+					recurse(currentNode.children[i]);
+				}
+				callback(currentNode);
+			})(this._root);
+		},
+
+		//广度首次遍历
+		traverseBF: function(callback) {
+			var queue = [];
+
+			queue.push(this._root);
+			var currentNode = queue.shift();
+			while (currentNode) {
+				for (var i = 0; i < currentNode.children.length; i++) {
+					queue.push(currentNode.children[i]);
+				}
+				callback(currentNode);
+				currentNode = queue.shift();
+			}
+		},
+
+		contains: function(callback, traversal) {
+			traversal.call(this, callback);
+		},
+
+		add: function(data, toData, tvaversal) {
+			var child = new Node(data),
+				parent = null,
+				callback = function(node) {
+					if (node.data === toData) {
+						parent = node;
+					}
+				};
+			this.contains(callback, tvaversal);
+			if (parent) {
+				parent.children.push(child);
+				child.parent = parent;
+				this._root.showDOM(data, parent, child);
+			} else {
+				throw new Error('不存在父节点');
+			}
+		},
+
+		remove: function(data, formData, tvaversal) {
+			var parent = null,
+				index,
+				callback = function(node) {
+					if (node.data === formData) {
+						parent = node;
+					}
+				};
+			this.contains(callback, tvaversal);
+			if (parent) {
+				index = this.getIndex(data, parent);
+				if (index === undefined) {
+					throw new Error('没有找到想要删除的元素');
+				} else {
+					this._root.delDOM(parent, index);
+					parent.children.splice(index, 1);
+				}
+
+			} else {
+				throw new Error('父节点不存在');
+			}
+		},
+
+		getIndex: function(data, parent) {
+			var index;
+			for (var i = 0; i < parent.children.length; i++) {
+				if (data === parent.children[i].data) {
+					index = i;
+				}
+			}
+			return index;
+		}
+	}
+
+	var root = new Tree('前端开发');
+	root.add('demo', '前端开发', root.traverseBF);
+	root.add('JS', '前端开发', root.traverseBF);
+	root.add('CSS', '前端开发', root.traverseBF);
+	root.add('HTML', '前端开发', root.traverseBF);
+	root.add('轮播图1', 'demo', root.traverseBF);
+	root.add('轮播图2', 'demo', root.traverseBF);
+	root.add('轮播图3', 'demo', root.traverseBF);
+	root.add('懒加载', 'demo', root.traverseBF);
+	root.add('瀑布流', 'demo', root.traverseBF);
+	root.add('AngularJS', 'JS', root.traverseBF);
+	root.add('React', 'JS', root.traverseBF);
+	root.add('ExtJS', 'JS', root.traverseBF);
+	root.add('NodeJS', 'JS', root.traverseBF);
+	root.add('CSS2.0', 'CSS', root.traverseBF);
+	root.add('CSS3.0', 'CSS', root.traverseBF);
+	root.add('Bootstrap', 'CSS', root.traverseBF);
