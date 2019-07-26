e6329748c93fe46d8529fe95709008db5ec0dad7
limenglin <limenglin@quanyou.com.cn>
2019-07-26 16:01:02 +0800

    'addtmlWebpackPlugin'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // webpack v4
-// const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
 // const webpack = require('webpack'); //访问内置的插件
 const path = require('path');
 
@@ -12,6 +12,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management' //如何解决index.html本来有HTML元素的情况
+    })
+  ],
   // plugins: [
   //   new webpack.optimize.UglifyJsPlugin(),
   //   new HtmlWebpackPlugin({template: './src/index.html'})
