406c5e03b9f129baed51f5c5b0f34d919332f19b
limenglin <limenglin@quanyou.com.cn>
2019-07-26 17:54:27 +0800

    'done-ev'

--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
+import { cube } from './math.js';
 export default function printMe() {
-    console.log('I get called from print.js!');
+    console.log('I get called from print.js!',cube);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack v4
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
-// const webpack = require('webpack'); //访问内置的插件
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack'); //访问内置的插件
 const path = require('path');
 
 module.exports = {
@@ -8,17 +9,31 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map', //代码错误检测追踪
+  devServer: {
+    contentBase: './dist', //实时重新加载
+    hot: true
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    publicPath: '/' //express webpack-dev-middleware publicPath 也会在服务器脚本用到，以确保文件资源能够在 http://localhost:3000 下正确访问
   },
+   module: {
+       rules: [
+           {
+             test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+         }
+       ]
+ },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management' //如何解决index.html本来有HTML元素的情况
-    })
+      template: 'src/index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin(),
-  //   new HtmlWebpackPlugin({template: './src/index.html'})
-  // ]
+    mode: "production"
 };
